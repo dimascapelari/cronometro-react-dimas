9babea4e9fa7eb0f08f42387e2500e200ca8c6e0
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-16 21:54:02 -0300

    up

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './Botao.module.scss';
+
+class Botao extends React.Component<{
+    children: string
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void
+}> {
+    render() {
+        const { type = "button", onClick } = this.props
+        return (
+            <button onClick={onClick} type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
